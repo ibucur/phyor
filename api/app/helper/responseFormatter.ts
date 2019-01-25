27b6709cce691f3aaf0bdd0af0136f5741f45d6a
Irinel BUCUR <irinel.bucur@gmail.com>
2019-01-25 20:23:05 +0200

    made a refactoring and made sure the json format is correct when no Accept header is sent

--- api/app/helper/responseFormatter.ts
+++ api/app/helper/responseFormatter.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from "express";
 import * as util from "util";
 import {isNullOrUndefined} from "util";
+import {Res} from "routing-controllers";
 const js2xmlparser = require("js2xmlparser");
 
 export class ResponseFormatter {
@@ -12,23 +13,28 @@ export class ResponseFormatter {
         return data;
     }
 
+    public static removeXMLTransformData(data: any): any {
+        if (Array.isArray(data)) {
+            for (let i = 0; i < data.length; i++) {
+                data[i] = ResponseFormatter.removeXMLTransformDetails(data[i]);
+                if (!isNullOrUndefined(data[i]['author'])) data[i]['author'] = ResponseFormatter.removeXMLTransformDetails(data[i]['author']);
+                if (!isNullOrUndefined(data[i]['genre'])) data[i]['genre'] = ResponseFormatter.removeXMLTransformDetails(data[i]['genre']);
+                if (!isNullOrUndefined(data[i]['publisher'])) data[i]['publisher'] = ResponseFormatter.removeXMLTransformDetails(data[i]['publisher']);
+                if (!isNullOrUndefined(data[i]['language'])) data[i]['language'] = ResponseFormatter.removeXMLTransformDetails(data[i]['language']);
+                if (!isNullOrUndefined(data[i]['currency'])) data[i]['currency'] = ResponseFormatter.removeXMLTransformDetails(data[i]['currency']);
+            }
+        }
+        else {
+            data = ResponseFormatter.removeXMLTransformDetails(data);
+        }
+        return data;
+    }
+
     public static response(res: any, req:any, data: any, status: number = 200, xmlRoot: string = 'root'): any {
         if (!req.headers["accept"] || isNullOrUndefined(req.headers["accept"]) || req.headers.accept == 'application/json' || req.headers.accept == '*/*') {
             res.setHeader('Content-Type', 'application/json');
             res.status(status);
-            if (Array.isArray(data)) {
-                for (let i = 0; i < data.length; i++) {
-                    data[i] = ResponseFormatter.removeXMLTransformDetails(data[i]);
-                    if (!isNullOrUndefined(data[i]['author'])) data[i]['author'] = ResponseFormatter.removeXMLTransformDetails(data[i]['author']);
-                    if (!isNullOrUndefined(data[i]['genre'])) data[i]['genre'] = ResponseFormatter.removeXMLTransformDetails(data[i]['genre']);
-                    if (!isNullOrUndefined(data[i]['publisher'])) data[i]['publisher'] = ResponseFormatter.removeXMLTransformDetails(data[i]['publisher']);
-                    if (!isNullOrUndefined(data[i]['language'])) data[i]['language'] = ResponseFormatter.removeXMLTransformDetails(data[i]['language']);
-                    if (!isNullOrUndefined(data[i]['currency'])) data[i]['currency'] = ResponseFormatter.removeXMLTransformDetails(data[i]['currency']);
-                }
-            }
-            else {
-                data = ResponseFormatter.removeXMLTransformDetails(data);
-            }
+            data = ResponseFormatter.removeXMLTransformData(data);
             return res.json(data);
         }
         else if (!isNullOrUndefined(req.headers["accept"]) && req.headers.accept == 'application/xml') {
@@ -39,13 +45,9 @@ export class ResponseFormatter {
         else {
             res.setHeader('Content-Type', 'application/json');
             res.status(status);
-            if (Array.isArray(data)) {
-                for (let i = 0; i < data.length; i++) {
-                    delete data[i]["="];
-                    data[i]["id"] = data[i]["@1"]["id"];
-                    delete data[i]["@1"];
-                }
-            }
+            
+            data = ResponseFormatter.removeXMLTransformData(data);
+
             return res.json(data);
         }
     }
